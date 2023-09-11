2eb2aa3c9f868c2b2c619e9f7b71c8c214c48221
Vitor Antonio Vieira da Silva <125030836+VitorAntonioVieira@users.noreply.github.com>
2023-09-10 21:03:42 -0300

    cccccc

--- paginainicial.php
+++ paginainicial.php
@@ -25,7 +25,7 @@
 
 <body>
     <nav>
-        <img id="logo" src="img/logos/1.png" alt="Logo">
+        <img id="logo" src="img/logos/5.png" alt="Logo">
         <div id="divBusca">
             <img src="img/2x/round_search_black_24dp.png" alt="">
             <input type="text" id="txtBusca" placeholder="Buscar por itens" />
@@ -52,9 +52,9 @@
             <?php
                 include 'includes/conexao.php';
 
-                $sql = "SELECT * FROM lanches";
-                $sql_exec = $mysqli->query($sql) or die($mysqli->error);
-                while($lanches = $sql_exec->fetch_assoc()){
+                $sql_l = "SELECT * FROM lanches";
+                $query_lanches = $mysqli->query($sql_l) or die($mysqli->error);
+                while($lanches = $query_lanches->fetch_assoc()){
             ?>
             <div class="fild">
                 <fieldset class="box">
@@ -64,159 +64,43 @@
                 </fieldset>
             </div>
             <?php } ?>
-            <!-- <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/xburguer.png" alt="Lanche xburguer">
-                    <h3>Simpleslândia</h3>
-                    <p>R$15,09</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/xveg.png" alt="Vegslândia">
-                    <h3>Vegslândia</h3>
-                    <p>R$30,09</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/xbacon.png" alt="Baconlândia">
-                    <h3>Baconlândia</h3>
-                    <p>R$35,09</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/ovolândia.png" alt="Baconlândia">
-                    <h3>Ovolândia</h3>
-                    <p>R$28,09</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/xfrango.png" alt="xfrango">
-                    <h3>X-Frango</h3>
-                    <p>R$30,09</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/xsalada.png" alt="xsalada">
-                    <h3>Salalândia</h3>
-                    <p>R$27,09</p>
-                </fieldset>
-            </div> -->
         </div>
     </div>
     <h1 class="grid-title">BEBIDAS</h1>
     <div class="grid-container">
         <div class="bebi">
-
+            <?php
+                $sql_b = "SELECT * FROM bebidas";
+                $query_bebidas = $mysqli->query($sql_b) or die($mysqli->error);
+                while($bebidas = $query_bebidas->fetch_assoc()){
+            ?>
             <div class="fild">
                 <fieldset class="box">
-                    <img class="produto" src="img/guarana.png" alt="GuaranaAntartica">
-                    <h3>Guaraná</h3>
-                    <p>R$5,00</p>
+                    <?php echo '<img class="produto" src="'.$bebidas['imagem'].'" alt="'.$bebidas['nome'].'">
+                    <h3>'.$bebidas['nome'].'</h3>
+                    <p>'.$bebidas['preco'].'</p>';?>
                 </fieldset>
             </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/cocacola.png" alt="CocaCola">
-                    <h3>Coca-Cola</h3>
-                    <p>R$6,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/sprite.png" alt="Sprite">
-                    <h3>Sprite</h3>
-                    <p>R$5,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/suco.png" alt="Suco morango">
-                    <h3>Morangolândia</h3>
-                    <p>R$10,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/GarrafaAgua.png" alt="Garrafa de água">
-                    <h3>Água</h3>
-                    <p>R$4,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/milkshake.png" alt="MilkShake de Chocolate">
-                    <h3>Shakelate</h3>
-                    <p>R$15,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/milkshake2.png" alt="MilkShake de Morango">
-                    <h3>Shakegolândia</h3>
-                    <p>R$15,00</p>
-                </fieldset>
-            </div>
+            <?php } ?>
         </div>
     </div>
     <h1 class="grid-title">PORÇÕES</h1>
     <div class="grid-container">
         <div class="porço">
+            <?php
+                $sql_p = "SELECT * FROM porcoes";
+                $query_porcoes = $mysqli->query($sql_p) or die($mysqli->error);
+                while($porcoes = $query_porcoes->fetch_assoc()){
+            ?>
             <div class="fild">
                 <fieldset class="box">
-                    <img class="produto" src="img/batatafrita.png" alt="Porção de Batata">
-                    <h3>Batatas</h3>
-                    <p>R$15,00</p>
+                    <?php echo '<img class="produto" src="'.$porcoes['imagem'].'" alt="'.$porcoes['nome'].'">
+                    <h3>'.$porcoes['nome'].'</h3>
+                    <p>'.$porcoes['preco'].'</p>';?>
                 </fieldset>
             </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/coxinhas.png" alt="Porção de Coxinhas">
-                    <h3>Coxinhas</h3>
-                    <p>R$17,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/pastel.png" alt="Porção de Pastéis">
-                    <h3>Pastéis</h3>
-                    <p>R$19,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/nuggets.png" alt="Porção de Nuggets">
-                    <h3>Nuggets</h3>
-                    <p>R$19,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/onionrings.png" alt="Porção de anel de cebola">
-                    <h3>Onion Rings</h3>
-                    <p>R$15,00</p>
-                </fieldset>
-            </div>
-            <div class="fild">
-                <fieldset class="box">
-                    <img class="produto" src="img/calabresa.png" alt="Porção de Calabresa">
-                    <h3>Calabresa</h3>
-                    <p>R$20,00</p>
-                </fieldset>
-            </div>
-            <div class="fildfinal">
-                <fieldset class="box">
-                    <img class="produto" src="img/azeitona.png" alt="Porção de azeitona">
-                    <h3>Azeitonas</h3>
-                    <p>R$10,00</p>
-                </fieldset>
-            </div>
+            <?php } ?>
         </div>
     </div>
 </body>
-
 </html>
